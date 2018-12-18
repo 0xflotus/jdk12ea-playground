5c2dcca00c27f758d71fbf8fc65b23d63011b3ac
0xflotus <0xflotus@gmail.com>
2018-12-18 18:45:59 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,5 @@
+echo "JDK12 Path?"
+
+read JDK12_PATH
+
+$JDK12_PATH/Contents/Home/bin/jshell --enable-preview $1
---
+++ switch.jsh
@@ -0,0 +1,3 @@
+System.out.println(switch(4-4){case 1, 2, 3 -> 1; case 0 -> 0; default -> 2;});
+
+/exit
